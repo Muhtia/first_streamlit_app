c2f521c940cc7044a6ba23dc9b54a1e305960f54
Muhtia <70850046+Muhtia@users.noreply.github.com>
2022-11-11 15:19:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
-streamlit.header('SMuhti's Fruit Advice!')
+streamlit.header('SMoohtis Fruit Advice!')
 try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
